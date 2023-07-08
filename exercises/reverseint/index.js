50c8d9f9941a01a1b320c7e6ebc5d9525a8c4032
Peris <joyperis11@gmail.com>
2023-07-08 11:36:24 +0300

    Reverse Int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let sign=Math.sign(n)
+    let reversed=n.toString().split('').reverse().join('');
+    return parseInt(reversed)*sign;
+}
 
 module.exports = reverseInt;
