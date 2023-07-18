813dd012ba634164120377fe0e11e1f2c7937881
Peris <joyperis11@gmail.com>
2023-07-18 15:02:41 +0300

    Array Chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    const chunkedArray=[];
+    
+    for(let i=0; i<array.length;i=i+size){
+        chunkedArray.push(array.slice(i,i+size))
+    }
+    return chunkedArray
+}
 
 module.exports = chunk;
